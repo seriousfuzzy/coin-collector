dc00e971401f7016ab957dc6ed77d2c9fbb39ca6
seriousfuzzy <seriousfuzzy1211@gmail.com>
2024-10-08 10:14:56 +0900

    add userEdit page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { AssetsViewPage } from "../pages/assets-view/assets-view"
 import { ArticlesViewPage } from "../pages/articles-view/articles-view"
 import { AboutPage } from "../pages/about/about"
 import { UserDetailPage } from "../pages/user-detail/user-detail"
-import { SignUpPage } from "../pages/sign-up/sign-up"
 import { UserArticlesViewPage } from "../pages/user-articles-view/user-articles-view"
 
 @Component({
@@ -17,29 +16,23 @@ import { UserArticlesViewPage } from "../pages/user-articles-view/user-articles-
 export class MyApp {
     @ViewChild(Nav) nav: Nav
 
-    rootPage: any = SignUpPage
-    userPages: Array<{ title: string, component: any }>
-    mainPages: Array<{ title: string, component: any }>
-    aboutPages: Array<{ title: string, component: any }>
+    rootPage: any = AssetsViewPage
+
+    pages: Array<{ title: string, component: any }>
 
     constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
         this.initializeApp()
 
         // used for an example of ngFor and navigation
-        this.mainPages = [
+        this.pages = [
             {title: 'Home', component: HomePage},
             {title: '総資産', component: AssetsViewPage},
             {title: 'キャッチアップ', component: ArticlesViewPage},
-        ]
-
-        this.userPages = [
             {title: 'プロフィール', component: UserDetailPage},
-            {title: 'あなたの投稿一覧', component: UserArticlesViewPage},
-        ]
-
-        this.aboutPages = [
+            {title: 'あなたの投稿一覧',component: UserArticlesViewPage},
             {title: 'このアプリについて', component: AboutPage}
         ]
+
     }
 
     initializeApp() {
